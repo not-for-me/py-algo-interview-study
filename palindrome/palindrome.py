a3e9d5890d07cc1759f97333d2098b335699bff3
woojin.joe <jwj0831@gmail.com>
2020-07-31 22:38:23 +0900

    [Palindrome] add type info in func signature

--- palindrome/palindrome.py
+++ palindrome/palindrome.py
@@ -1,5 +1,7 @@
 
-def check(sentence) -> bool:
+# time complexity: O(N)
+# space complexity: O(N)
+def check(sentence: str) -> bool:
     chars = [c.lower() for c in sentence if c.isalnum()]
     for i in range( len(chars) // 2 ):
         if chars[i] == chars[len(chars) - i - 1]:
